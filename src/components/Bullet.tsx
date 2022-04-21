0bddc3c70accf0d6dca580f84f99c0c927d4c57d
Itay Cohen <itaycs2407@gmail.com>
2022-04-21 22:51:12 +0300

    change personal consult to be from locale add Bullet component

---
+++ src/components/Bullet.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import BulletMarker from "../assets/Bullet.jpeg";
+import styled from "@emotion/styled/macro";
+
+interface Props {
+  title: string;
+}
+
+const Bullet: React.FC<Props> = ({ title }) => (
+  <Container>
+    <StyledBulletMarker src={BulletMarker} alt="BulletMarker" />
+    <BulletTitle>{title}</BulletTitle>
+  </Container>
+);
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+`;
+
+const BulletTitle = styled.div``;
+
+const StyledBulletMarker = styled.img`
+  margin-left: 10px;
+`;
+
+export default Bullet;
--- src/pages/consult.tsx
+++ src/pages/consult.tsx
@@ -5,62 +5,47 @@ import Intro1 from "../assets/personalConsult/intro1.png";
 import Intro2 from "../assets/personalConsult/intro2.png";
 import Intro3 from "../assets/personalConsult/intro3.png";
 import Background from "../assets/personalConsult/background.jpeg";
+import { locale } from "../locale/locale";
+import Bullet from "../components/Bullet";
 
 interface Props {}
 
 const Consult: React.FC<Props> = () => (
-  <Container>
-    <ImageContainer>
-      <Image src={Intro1} alt="cover" />
-      <Image src={Intro3} alt="cover" />
-      <Image src={Intro2} alt="cover" />
-    </ImageContainer>
-    <h1>הדרכת הורים פרטנית</h1>
-    <Info>
-      <p>
-        משפחה היא המקום בו אמורים להרגיש בטוח, לחלוק חוויות ולצבור חויות
-        משותפות.
-      </p>
-      <p>
-        אני מאמינה בהדרכה פרטית המותאמת לערכי המשפחה בתהליך ממוקד הנותן מענה
-        לצרכיהם ולרגשותיהם של כלל חברי המשפחה.
-      </p>
-      <p>במסגרת תהליך היעוץ אני נותנת מענה במגון תחומים: </p>
-      <p>
-        <ul>
-          <li>
-            שיפור האווירה והתקשורת במשפחה- איך להכניס לבית שיתוף חיוביות ושמחה.
-          </li>
-          <li>
-            סמכות הורית-, איך לחזק את המערכת היחסים בין ההורים לילדים וליצור
-            סביבה משפחתית מכבדת, מאפשרת וגם מחנכת וסמכותית.
-          </li>
-          <li>
-            שיפור היחסים בתוך המשפחה-חיכוכים מתח וקשיים בינאישיים המשפיעים על
-            המשפחה כולה.
-          </li>
-          <li>
-            מתן מענה באתגרים שונים בגידול הילדים כגון: מריבות בן אחים, גמילה,
-            כניסה למסגרות חדשות ועוד'.
-          </li>
-          <li>התמודדות עם התנהגויות שליליות של הילדים.</li>
-          <li>תמיכה ויעוץ להורים חדשים</li>
+  <div
+    style={{
+      backgroundImage: "url(" + Background + ")",
+      backgroundSize: "auto auto",
+    }}
+  >
+    <Container>
+      <ImageContainer>
+        <Image src={Intro1} alt="Intro1" />
+        <Image src={Intro3} alt="Intro3" />
+        <Image src={Intro2} alt="Intro2" />
+      </ImageContainer>
+      <h1>{locale("personalConsultHeader")}</h1>
+      <ContentContainer>
+        {locale("personalConsultSentences").map(
+          (sentence: string, index: number) => (
+            <p key={index}>{sentence}</p>
+          )
+        )}
 
-          <li>
-            שיפור האווירה והתקשורת במשפחה- איך להכניס לבית שיתוף חיוביות ושמחה.
-          </li>
-          <li>הכנה להורות –הכנה לקראת המעבר מזוגיות להורות .</li>
-        </ul>
-        יחד , נזהה את הקשיים הקיימים, נייצר פתרונות מעשיים המתאימים לכם וניצור
-        שלווה ושמחה בבית
-      </p>
-    </Info>
-    <SumUpContainer>
-      <SumUpItem> קצר מועד</SumUpItem>
-      <SumUpItem>בזום או בקלינקה</SumUpItem>
-      <SumUpItem>פרקטי ומותאם לערכי המשפחה</SumUpItem>
-    </SumUpContainer>
-  </Container>
+        {locale("personalConsultBullets").map(
+          (bullet: string, index: number) => (
+            <Bullet key={index} title={bullet} />
+          )
+        )}
+
+        <p>{locale("personalConsultSumUp")}</p>
+      </ContentContainer>
+      <SumUpContainer>
+        <SumUpItem> קצר מועד</SumUpItem>
+        <SumUpItem>בזום או בקלינקה</SumUpItem>
+        <SumUpItem>פרקטי ומותאם לערכי המשפחה</SumUpItem>
+      </SumUpContainer>
+    </Container>
+  </div>
 );
 
 const Container = styled.div`
@@ -69,11 +54,6 @@ const Container = styled.div`
   align-items: center;
   gap: 10px;
   animation: ${Fadein} 1s ease-in-out;
-  height: 400px;
-  width: 100%;
-  background: "url(" + Background + ")";
-  background-repeat: no-repeat, no-repeat;
-  background-position: right, left;
 `;
 
 const ImageContainer = styled.div`
@@ -86,7 +66,7 @@ const Image = styled.img`
   box-shadow: 2px 4px 10px rgba(0, 0, 0, 0.5);
 `;
 
-const Info = styled.p`
+const ContentContainer = styled.p`
   width: 80%;
   text-align: right;
 `;
